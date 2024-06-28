ba334f585c452ae51ea308c9c670e12506531152
Ziyan <ziyan.zhuang@h3platform.com>
2024-06-28 17:14:39 +0800

    2024-6-28 init form view

--- WebView2WinFormsApp/Form1.Designer.vb
+++ WebView2WinFormsApp/Form1.Designer.vb
@@ -23,8 +23,10 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MainWebView2 = New Microsoft.Web.WebView2.WinForms.WebView2()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Navigate_to_url_Button = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.url_TextBox = New System.Windows.Forms.TextBox()
         CType(Me.MainWebView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -33,47 +35,67 @@ Partial Class Form1
         Me.MainWebView2.AllowExternalDrop = True
         Me.MainWebView2.CreationProperties = Nothing
         Me.MainWebView2.DefaultBackgroundColor = System.Drawing.Color.White
-        Me.MainWebView2.Location = New System.Drawing.Point(12, 66)
+        Me.MainWebView2.Location = New System.Drawing.Point(12, 12)
         Me.MainWebView2.Name = "MainWebView2"
-        Me.MainWebView2.Size = New System.Drawing.Size(896, 544)
+        Me.MainWebView2.Size = New System.Drawing.Size(877, 752)
         Me.MainWebView2.Source = New System.Uri("https://www.bing.com/", System.UriKind.Absolute)
         Me.MainWebView2.TabIndex = 0
         Me.MainWebView2.ZoomFactor = 1.0R
         '
-        'Button1
+        'Navigate_to_url_Button
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 24)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 29)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Navigate_to_url_Button.Location = New System.Drawing.Point(736, 770)
+        Me.Navigate_to_url_Button.Name = "Navigate_to_url_Button"
+        Me.Navigate_to_url_Button.Size = New System.Drawing.Size(153, 29)
+        Me.Navigate_to_url_Button.TabIndex = 1
+        Me.Navigate_to_url_Button.Text = "前往"
+        Me.Navigate_to_url_Button.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(267, 24)
+        Me.Button2.Location = New System.Drawing.Point(895, 770)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.Size = New System.Drawing.Size(529, 29)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(895, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(529, 752)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'url_TextBox
+        '
+        Me.url_TextBox.Location = New System.Drawing.Point(12, 772)
+        Me.url_TextBox.Name = "url_TextBox"
+        Me.url_TextBox.Size = New System.Drawing.Size(718, 27)
+        Me.url_TextBox.TabIndex = 4
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(920, 622)
+        Me.ClientSize = New System.Drawing.Size(1436, 811)
+        Me.Controls.Add(Me.url_TextBox)
+        Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Navigate_to_url_Button)
         Me.Controls.Add(Me.MainWebView2)
         Me.Name = "Form1"
         Me.Text = "WebView2Form"
         CType(Me.MainWebView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents MainWebView2 As Microsoft.Web.WebView2.WinForms.WebView2
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Navigate_to_url_Button As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents url_TextBox As TextBox
 End Class
--- WebView2WinFormsApp/Form1.vb
+++ WebView2WinFormsApp/Form1.vb
@@ -1,30 +1,25 @@
-﻿Imports System.IO
-Imports System.IO.Pipes
+﻿
+Imports Microsoft.Web.WebView2.Core
 
 Public Class Form1
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Debug.WriteLine("Form Load")
+        url_TextBox.Text = "https://www.google.com.tw/"
     End Sub
 
+
+
+    Private Sub Navigate_to_url_Button_Click(sender As Object, e As EventArgs) Handles Navigate_to_url_Button.Click
+
+        MainWebView2.CoreWebView2.Navigate(url_TextBox.Text)
+
+    End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim pipeName As String = "MyNamedPipe"
-        Using pipeClient As New NamedPipeClientStream(".", pipeName, PipeDirection.In)
-            Debug.WriteLine("tring to connect pipe...")
-            pipeClient.Connect()
-            Debug.WriteLine("connected to pipe ...")
-
-            Dim reader As New StreamReader(pipeClient)
-            Dim message As String = reader.ReadLine()
-
-            Debug.WriteLine("received : " & message)
-            'MainWebView2.CoreWebView2.Navigate("https://baidu.com/")
-
-        End Using
+        Dim jsScript As String = "alert('Hello, this is an alert!')"
+        MainWebView2.CoreWebView2.ExecuteScriptAsync(jsScript)
     End Sub
-
-
 
 End Class
