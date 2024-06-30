fd01035a63efceef3c78aaeb8e11c88a499b28a6
ChuangZiyan <ziyan.zhuang@h3platform.com>
2024-06-30 22:20:48 +0800

    2024-6-30

--- WebView2WinFormsApp/Form1.Designer.vb
+++ WebView2WinFormsApp/Form1.Designer.vb
@@ -1,4 +1,6 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿Imports Microsoft.Web.WebView2.Core
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,80 +24,78 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.MainWebView2 = New Microsoft.Web.WebView2.WinForms.WebView2()
-        Me.Navigate_to_url_Button = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.url_TextBox = New System.Windows.Forms.TextBox()
-        CType(Me.MainWebView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'MainWebView2
-        '
-        Me.MainWebView2.AllowExternalDrop = True
-        Me.MainWebView2.CreationProperties = Nothing
-        Me.MainWebView2.DefaultBackgroundColor = System.Drawing.Color.White
-        Me.MainWebView2.Location = New System.Drawing.Point(12, 12)
-        Me.MainWebView2.Name = "MainWebView2"
-        Me.MainWebView2.Size = New System.Drawing.Size(877, 752)
-        Me.MainWebView2.Source = New System.Uri("https://www.bing.com/", System.UriKind.Absolute)
-        Me.MainWebView2.TabIndex = 0
-        Me.MainWebView2.ZoomFactor = 1.0R
-        '
-        'Navigate_to_url_Button
-        '
-        Me.Navigate_to_url_Button.Location = New System.Drawing.Point(736, 770)
-        Me.Navigate_to_url_Button.Name = "Navigate_to_url_Button"
-        Me.Navigate_to_url_Button.Size = New System.Drawing.Size(153, 29)
-        Me.Navigate_to_url_Button.TabIndex = 1
-        Me.Navigate_to_url_Button.Text = "前往"
-        Me.Navigate_to_url_Button.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(895, 770)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(529, 29)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(895, 12)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(529, 752)
-        Me.RichTextBox1.TabIndex = 3
-        Me.RichTextBox1.Text = ""
-        '
-        'url_TextBox
-        '
-        Me.url_TextBox.Location = New System.Drawing.Point(12, 772)
-        Me.url_TextBox.Name = "url_TextBox"
-        Me.url_TextBox.Size = New System.Drawing.Size(718, 27)
-        Me.url_TextBox.TabIndex = 4
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1436, 811)
-        Me.Controls.Add(Me.url_TextBox)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Navigate_to_url_Button)
-        Me.Controls.Add(Me.MainWebView2)
-        Me.Name = "Form1"
-        Me.Text = "WebView2Form"
-        CType(Me.MainWebView2, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        Navigate_to_url_Button = New Button()
+        Button2 = New Button()
+        RichTextBox1 = New RichTextBox()
+        url_TextBox = New TextBox()
+        WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Navigate_to_url_Button
+        ' 
+        Navigate_to_url_Button.Location = New Point(736, 770)
+        Navigate_to_url_Button.Name = "Navigate_to_url_Button"
+        Navigate_to_url_Button.Size = New Size(153, 29)
+        Navigate_to_url_Button.TabIndex = 1
+        Navigate_to_url_Button.Text = "前往"
+        Navigate_to_url_Button.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(895, 770)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(529, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(895, 12)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(529, 752)
+        RichTextBox1.TabIndex = 3
+        RichTextBox1.Text = ""
+        ' 
+        ' url_TextBox
+        ' 
+        url_TextBox.Location = New Point(12, 772)
+        url_TextBox.Name = "url_TextBox"
+        url_TextBox.Size = New Size(718, 27)
+        url_TextBox.TabIndex = 4
+        ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.WhiteSmoke
+        WebView21.Location = New Point(12, 12)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(877, 752)
+        WebView21.TabIndex = 5
+        WebView21.ZoomFactor = 1.0R
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9.0F, 19.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1436, 811)
+        Controls.Add(WebView21)
+        Controls.Add(url_TextBox)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Navigate_to_url_Button)
+        Name = "Form1"
+        Text = "WebView2Form"
+        CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
-
-    Friend WithEvents MainWebView2 As Microsoft.Web.WebView2.WinForms.WebView2
     Friend WithEvents Navigate_to_url_Button As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents RichTextBox1 As RichTextBox
     Friend WithEvents url_TextBox As TextBox
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+
 End Class
--- WebView2WinFormsApp/Form1.vb
+++ WebView2WinFormsApp/Form1.vb
@@ -1,25 +1,74 @@
 ﻿
 Imports Microsoft.Web.WebView2.Core
+Imports Microsoft.Web.WebView2.WinForms
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports OpenQA.Selenium.Edge
+Imports OpenQA.Selenium.Remote
+Imports Selenium
+Imports WebDriverManager
+Imports WebDriverManager.DriverConfigs.Impl
+Imports WebDriverManager.Helpers
 
 Public Class Form1
 
+    'Public edgeDriver As IWebDriver
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private debugPort As Integer = 9222 ' 這是我們將要使用的調試埠
+    Private environment As CoreWebView2Environment
+
+    Private Async Function InitializeWebView2() As Task
+        environment = Await CoreWebView2Environment.CreateAsync(Nothing, Nothing, New CoreWebView2EnvironmentOptions("--remote-debugging-port=" & debugPort))
+        Await WebView21.EnsureCoreWebView2Async(environment)
+
+    End Function
+
+
+    Private Async Sub Form1_Load_2(sender As Object, e As EventArgs) Handles MyBase.Load
         Debug.WriteLine("Form Load")
         url_TextBox.Text = "https://www.google.com.tw/"
+        Await InitializeWebView2()
+
+        Dim driverManager = New DriverManager()
+        'driverManager.SetUpDriver(New EdgeConfig(), VersionResolveStrategy.MatchingBrowser) 'automatically download a chromedriver.exe matching the version of the browser
+        driverManager.SetUpDriver(New EdgeConfig())
+
+
+        Dim options As EdgeOptions = New EdgeOptions()
+        options.DebuggerAddress = "localhost:9222"
+
+
+        Dim environment As CoreWebView2Environment = CoreWebView2Environment.CreateAsync().Result
+        Dim edgeVersion As String = environment.BrowserVersionString
+
+        ' 輸出版本信息
+        Debug.WriteLine("WebView2 使用的 Microsoft Edge 版本： " & edgeVersion)
+
+        ' 創建RemoteWebDriver並指定選項
+        Dim driver As IWebDriver = New RemoteWebDriver(options)
+        ' 打開網頁
+        driver.Navigate().GoToUrl("http://www.example.com")
+
+        driver.Quit()
+
+
     End Sub
 
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim jsScript As String = "alert('Hello, this is an alert!')"
+        'MainWebView2.CoreWebView2.ExecuteScriptAsync(jsScript)
+
+        'edgeDriver.Navigate.GoToUrl("https://google.com.tw/")
+    End Sub
 
 
     Private Sub Navigate_to_url_Button_Click(sender As Object, e As EventArgs) Handles Navigate_to_url_Button.Click
 
-        MainWebView2.CoreWebView2.Navigate(url_TextBox.Text)
+
+
+        'edgeDriver.Navigate.GoToUrl("https://www.google.com/")
 
     End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim jsScript As String = "alert('Hello, this is an alert!')"
-        MainWebView2.CoreWebView2.ExecuteScriptAsync(jsScript)
-    End Sub
-
 End Class
